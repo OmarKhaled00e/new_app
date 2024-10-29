e9734a004123a343724ec5422f39e3fb22e2446d
OmarKhaled00e <ok4966541@gmail.com>
2024-10-29 18:42:32 +0300

    News api local data

---
+++ lib/widgets/news_tile.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class NewsTile extends StatelessWidget {
+  const NewsTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(6),
+          child: Image.network(
+            'https://gratisography.com/wp-content/uploads/2024/10/gratisography-cool-cat-800x525.jpg',
+            height: 200,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+        ),
+        const SizedBox(
+          height: 12,
+        ),
+      ],
+    );
+  }
+}
